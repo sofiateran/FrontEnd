10603f9786cdc75b7c2b1626e145afcfcd0ac79e
sofiateran <sofiateran.dh@gmail.com>
2022-11-07 17:19:14 -0300

    ej mesa

---
+++ src/mesaTrabajo/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./card"
+import "./App.css"
+function App() {
+    
+    return (
+      <div className="App">
+          <h1>Carga de Estudiantes</h1>
+        <Card/>
+        <form></form>
+      </div>
+    )
+  }
+  
+  export default App
